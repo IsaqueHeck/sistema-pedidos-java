0698d30f9238b4c893dc12b88502db09f68682c7
Isaque Heck <heckisaque@gmail.com>
2026-06-11 15:37:12 -0300

    feat: adiciona tratamento global de exceçcoes

---
+++ src/main/java/com/isaque/sistemapedidos/exceptionhandler/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.isaque.sistemapedidos.exceptionhandler;
+
+import com.isaque.sistemapedidos.exceptions.ProdutoDuplicadoException;
+import com.isaque.sistemapedidos.exceptions.ProdutoNaoEncontradoException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ProdutoNaoEncontradoException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String tratarProdutoNaoEncontrado(ProdutoNaoEncontradoException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(ProdutoDuplicadoException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public String tratarProdutoDuplicado(ProdutoDuplicadoException ex) {
+        return ex.getMessage();
+    }
+
+}
